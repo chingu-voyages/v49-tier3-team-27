44b75f1999188d3c09a0c6c1d609042db47f1ce9
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-06-09 01:48:52 +0530

    creating events api

--- nest-ring-app/lib/utils.ts
+++ nest-ring-app/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function tryParseJSON(jsonString: string) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    return jsonString;
+  }
+}
+
+/* form data */
+export function parseEventFormData(formData: FormData) {
+  const formDataObject: { [key: string]: any } = {};
+  formData.forEach((val, key) => {
+    formDataObject[key] = typeof val === "string" ? tryParseJSON(val) : val;
+  });
+  return formDataObject;
+}
