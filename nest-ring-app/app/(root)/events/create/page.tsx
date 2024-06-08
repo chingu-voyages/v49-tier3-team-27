c5fe2ee19e633b2900fab61074b007bc4b80b5c8
Jaweki <werukioni@gmail.com>
2024-06-08 20:47:47 +0300

    added step 3 initial UI

--- nest-ring-app/app/(root)/events/create/page.tsx
+++ nest-ring-app/app/(root)/events/create/page.tsx
@@ -5,7 +5,7 @@ import Step1 from "./ui/steps/Step1";
 import PrevPageBtn from "../../order-meal/[searchTerm]/ui/PrevPageBtn";
 import { CreateEventContext } from "./ui/CreateEventContext";
 import Step2 from "./ui/steps/Step2";
-import { useSession } from "next-auth/react";
+import Step3 from "./ui/steps/Step3";
 
 const CreateEventMainPage = () => {
   const { activeStep } = useContext(CreateEventContext);
@@ -19,6 +19,7 @@ const CreateEventMainPage = () => {
       {/* 2. Image upload, event Date and time, Event location */}
       {activeStep === "step_2" && <Step2 />}
       {/* 3. Invite guests */}
+      {activeStep === "step_3" && <Step3 />}
       {/* 4. Book event Dishes */}
       {/* 5. Preview of event creation and set to create.*/}
     </main>
--- nest-ring-app/app/(root)/events/create/ui/steps/Step3.tsx
+++ nest-ring-app/app/(root)/events/create/ui/steps/Step3.tsx
@@ -4,10 +4,13 @@ const Step3 = () => {
     <section className="w-full flex flex-col gap-4 p-5">
       {/* Introductory message */}
       <h1 className="text-xl font-semibold font-serif text-figma-brown">
-        Expressive Image, Event Date, time and Location:
+        Invite your Guests and Monetize your event,
       </h1>
 
       {/* Invite the guests */}
+      <div></div>
+
+      {/* Monetization */}
     </section>
   );
 };
