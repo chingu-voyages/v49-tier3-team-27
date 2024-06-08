7aab5f77ad12cb0feb9dc17c6e8796640c5dfdc2
Jaweki <werukioni@gmail.com>
2024-06-09 00:43:08 +0300

    create new event UI, search user in db and show result in ui done.

--- nest-ring-app/app/(root)/events/create/ui/steps/Step3.tsx
+++ nest-ring-app/app/(root)/events/create/ui/steps/Step3.tsx
@@ -1,16 +1,79 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AsyncSelect from "react-select";
 import { CreateEventContext } from "../CreateEventContext";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+import { EventUserType } from "../interface";
+
+const DropDownUser = ({
+  imageUrl,
+  usersName,
+}: Readonly<{ imageUrl: string | null; usersName: string }>) => {
+  return (
+    <div className=" flex flex-row items-center gap-3">
+      <Image
+        src={imageUrl || "/random-images/profile-avatar.png"}
+        alt=""
+        width={20}
+        height={20}
+        className="rounded-full border"
+      />
+      <span className=" text-md font-bold ">{usersName}</span>
+    </div>
+  );
+};
 
 /* 3. Invite guests */
 const Step3 = () => {
   const { step3Values, updateActiveStep } = useContext(CreateEventContext);
+  const [foundUserObjs, setFoundUserObjs] = useState<EventUserType[]>([]);
+  const [loadingOptions, setLoadingOptions] = useState<any>([]);
+  const [keyword, setKeyword] = useState("");
 
   const handleNext = () => {
     updateActiveStep("step_4");
   };
+
+  const fetchUserObj = async (name: string) => {
+    try {
+      const response = await fetch(
+        `/api/events/create/filter-guests?name=${name}`
+      );
+
+      if (response.status === 200) {
+        const userObjs = (await response.json()) as EventUserType[];
+        setFoundUserObjs([...userObjs]);
+      } else {
+        toast({
+          title: "Alert!",
+          description: "Internal System Error. Contact System Develovpers.",
+        });
+      }
+    } catch (error) {
+      console.error("Create Event Tab Error: Failed to fetch users", error);
+      toast({
+        title: "Guest Lookup Failed!",
+        description: "Your Disconnected from the Internet.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const options = foundUserObjs.map((user) => ({
+      value: user.name,
+      label: <DropDownUser imageUrl={user.imageUrl} usersName={user.name} />,
+    }));
+
+    setLoadingOptions(options);
+  }, [foundUserObjs]);
+
+  useEffect(() => {
+    const debounce = setTimeout(() => fetchUserObj(keyword), 2000);
+    return () => clearTimeout(debounce);
+  }, [keyword]);
+
   return (
     <section className="w-full flex flex-col gap-4 p-5">
       {/* Introductory message */}
@@ -24,23 +87,12 @@ const Step3 = () => {
           Search a person's name to add them as your guest. They'll be notified.
         </span>
         <AsyncSelect
-          options={[
-            {
-              value: "item 1",
-              label: (
-                <button className=" flex flex-row items-center justify-start gap-3">
-                  <Image
-                    src={"/random-images/profile-avatar.png"}
-                    alt=""
-                    width={20}
-                    height={20}
-                    className="border rounded-full "
-                  />
-                  <span>Item 1</span>
-                </button>
-              ),
-            },
-          ]}
+          loadingMessage={() => <span>Searching...</span>}
+          onInputChange={(newValue) => {
+            console.log(newValue);
+            setKeyword(newValue);
+          }}
+          options={loadingOptions}
         />
         {/* selected guests */}
         <div className=" w-full border rounded-lg p-3 flex flex-col gap-2 mt-3">
--- nest-ring-app/app/api/events/create/filter-guests/route.ts
+++ nest-ring-app/app/api/events/create/filter-guests/route.ts
@@ -1,13 +1,24 @@
-import { NextRequest } from "next/server";
+import User from "@/lib/models/User";
+import dbConnect from "@/lib/mongo";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest) {
+export async function GET(req: NextRequest) {
+  try {
+    const keyword = req.nextUrl.searchParams.get("name");
 
-    try{
-        const keyword = req.nextUrl.searchParams
-        console.log('The keyword is : ', keyword);
-        
-    } catch(err) {
-        console.error(err)
+    let result = [];
+    if (keyword) {
+      await dbConnect();
+
+      const keywordRegex = new RegExp("." + "*" + keyword);
+      result = await User.find({
+        name: { $regex: keywordRegex, $options: "si" },
+      });
     }
-    
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ status: 500 });
+  }
 }
